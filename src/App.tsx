cdfc101679218c66a3b25d3453f8f0a56565a336
Arthur Alves <52552691+arthurMSA@users.noreply.github.com>
2024-05-04 01:37:32 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,28 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { IDBPDatabase, openDB } from 'idb'
+import { useState } from 'react'
+import './App.css'
+import { openDB } from 'idb'
 
 
 function App() {
-  const [beatleName, setBeatleName] = useState('')
-  const [results, setResults] = useState([''])
-  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [results, setResults] = useState<any[] | null>(null)
 
-  let db: IDBPDatabase;
-
-function showResult(str: string) {
-  console.log(str)
-}
-
-async function createDB() {
-  // Using https://github.com/jakearchibald/idb
-  try {
-    db = await openDB('bands', 1, {
-      upgrade(db, oldVersion, newVersion, transaction) {
-         const store = db.createObjectStore('beatles', {
-          autoIncrement: true,
-         });
-         store.createIndex('age', 'age');
-         showResult("Database and Data store created");
-     }
-    });
-    showResult("Database opened.");
-  } catch (e) {
-    console.log(e)
-  }
+function getDB() {
+  return openDB('bands', 1, {
+    upgrade(db, oldVersion, newVersion, transaction) {
+       const store = db.createObjectStore('images', {
+        autoIncrement: true,
+       })
+       store.createIndex('image', 'image')
+   }
+  })
 }
 
 async function getData() {
-    if (db==undefined) {
-      showResult("Database is closed");
-      return;
-    }
-  
-  const tx = await db.transaction('beatles', 'readonly')
-  const store = tx.objectStore('beatles');
+  const db = await getDB()
+  const tx = await db.transaction('images', 'readonly')
+  const store = tx.objectStore('images')
   const value = await store.getAll() || []
   setResults(value)
-  if (value) {
-    showResult("Data from DB: " + JSON.stringify(value))
-  } else {
-    showResult("There is no beatle with such name in store")
-  }
 }
 
 function handleImageUpload(event: any) {
@@ -61,66 +36,41 @@ function handleImageUpload(event: any) {
 }
 
 async function addData(data: any) {
-  if (db==undefined) {
-    showResult("Database is closed")
-    return;
-  }  
-  try {     
-    const beatle = {
-      name: beatleName,
-      nickname: 'The number 1',
-      age: 58,
-    };
-        
-    const tx = await db.transaction('beatles', 'readwrite');
-    
-    // Check first if the key already exists in store
-    const store = tx.objectStore('beatles');
-    // const beatleInStore = await store.get(beatleName)
-    // if (beatleInStore) {
-    //   showResult("Beatle is already in store");
-    //   return;
-    // }
-    // The key doesn't exist, so we add it to the store
-    console.log(data)
-    await store.add(data);
-    await tx.commit;
-    showResult("Beatle added to the database");
-
+  const db = await getDB()
+  try {
+    const tx = await db.transaction('images', 'readwrite')
+    const store = tx.objectStore('images')
+    await store.add(data)
+    await tx.commit
+    getData()
   } catch (e) {
     console.log(e)
-    showResult("Error while saving data to DB: ")
   }
 }
-  return (
-    <div className="App">
-      <div className="container">
-      <h1>
-        IndexedDB
-      </h1>
-      <p className="instructions">
-        Check within your browser developer tools IndexedDB contents.
-      </p>
-      <label>Beatle name
-        <input id="beatleName" placeholder="Enter a beatle name" onChange={(event) => setBeatleName(event.target.value)}/>
-      </label>
-
-      
-      <section className="toolbar">
-        <button id="create" onClick={createDB}>Open Database</button>
-        <button id="save" onClick={addData}>Save Beatle</button>
-        <button id="get" onClick={getData}>Get Beatle</button>
-        <button id="delete">Delete Beatle</button>
-      </section>
-      <input
-        type="file"
-        name="myImage"
-        onChange={(event) => handleImageUpload(event)}
-        ></input><br/>
-        <img src={results[1]}></img>
-    </div>
-    </div>
-  );
+  if (window.matchMedia('(display-mode: standalone)').matches) {
+    getData()
+    return (
+      <div className="App">
+        <div className="container">
+          <input
+            type="file"
+            name="myImage"
+            onChange={(event) => handleImageUpload(event)}
+            ></input><br/>
+            { results !== null ? results.map((image, index) =>
+                <img width={200} height={200} src={image} key={index}></img>
+              ) : 'Nenhuma imagem foi carregada'
+            }
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className="App">
+        INSTRUÇÃO PARA ADICIONAR O APP A TELA INICIAL
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
