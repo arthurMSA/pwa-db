6421e40f63b8bb85930036a0b584563cce6abb57
Arthur Alves <52552691+arthurMSA@users.noreply.github.com>
2024-05-04 00:48:23 -0300

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
-import { set, get } from 'idb-keyval';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createDB } from './serviceWorker';
-import ReactDOM from "react-dom/client";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App/>)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
-// createDB();
-
-// async function testIDBKeyval() {
-//     await set('hello', 'world');
-//     const whatDoWeHave = await get('hello');
-//     console.log(`When we queried idb-keyval for 'hello', we found: ${whatDoWeHave}`);
-// }
-
-// testIDBKeyval();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
